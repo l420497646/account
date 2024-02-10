be406aeb90ac16fa8505dcd037450be03e638040
l420497646 <liusixuan1996@163.com>
2024-02-11 00:53:20 +0800

    account练习flask

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+@app.route('/register',methods=['GET'])
+def register():
+    return render_template('register.html')
+
+@app.route('/post/reg',methods=['POST'])
+def post_reg():
+    print(request.form)
+    user = request.form.get("uu")
+    password = request.form.get("pp")
+    gender = request.form.get("gender")
+    hobby_list = request.form.getlist("hobby")
+    city = request.form.get("city")
+    skill = request.form.getlist("skill")
+    more = request.form.get("more")
+    return "注册成功"
+
+if __name__ == "__main__":
+    app.run()
